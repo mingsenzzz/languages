5b9db3528831c9c8cf3b441de42b65814593d2df
mingsenzzz <1255678570@qq.com>
2021-07-14 15:08:18 +0800

    add；定制化主题

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,27 @@
+const path = require("path");
+const bol = true;
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'zh',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
-    }
-  }
-}
+      locale: "zh",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false,
+    },
+  },
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          hack:
+            bol &&
+            `true; @import "${path.join(
+              __dirname,
+              "./src/common/customizeTheme.less"
+            )}";`,
+        },
+      },
+    },
+  },
+};
